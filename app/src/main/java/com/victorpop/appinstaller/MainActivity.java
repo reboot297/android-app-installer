4522afdaec4ca24b14812062408fc38980deaeb1
Reboot297 <reboot297@gmail.com>
2022-08-12 20:56:09 +0300

    created empty project

---
+++ app/src/main/java/com/victorpop/appinstaller/MainActivity.java
@@ -0,0 +1,22 @@
+package com.victorpop.appinstaller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.victorpop.appinstaller.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+    
+}
